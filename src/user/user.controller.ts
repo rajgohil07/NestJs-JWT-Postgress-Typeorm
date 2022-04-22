75f317dc91bed13512605773807e52864ef0b5e2
rajgohil07 <raj.famous007@gmail.com>
2022-04-22 18:54:35 +0530

    register and login system API

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // register the user into the database
+  @Post('create')
+  registerUser(
+    @Body('Name') Name: string,
+    @Body('Email') Email: string,
+    @Body('Password') Password: string,
+  ) {
+    return this.userService.registerUser(Name, Email, Password);
+  }
+
+  // login into system
+  @Post('login')
+  loginSystem(
+    @Body('Email') Email: string,
+    @Body('Password') Password: string,
+  ) {
+    return this.userService.loginSystem(Email, Password);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import bcrypt from 'bcryptjs';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  // register the user into the database
+  async registerUser(Name: string, Email: string, Password: string) {
+    const lowerEmail = Email.toLowerCase();
+    const hashPassword = await bcrypt.hash(Password, 10);
+    const CreateUserObject = {
+      Name,
+      Email,
+      Password: hashPassword,
+    };
+    const findEmailExist: UserEntity = await this.userRepository.findOne({
+      where: { Email: lowerEmail },
+    });
+    // if email exist in the database throw error
+    if (findEmailExist) {
+      throw new BadRequestException(
+        'This Email is already exist in the system',
+      );
+    }
+    const createUser = this.userRepository.create(CreateUserObject);
+    return this.userRepository.save(createUser);
+  }
+
+  // login into system
+  async loginSystem(Email: string, Password: string) {
+    const lowerEmail = Email.toLowerCase();
+    const findData = await this.userRepository.findOne({
+      where: { Email: lowerEmail },
+    });
+    if (!findData) {
+      throw new NotFoundException('Provided email does not found!');
+    }
+    const validPassword = await bcrypt.compare(Password, findData.Password);
+    if (validPassword) {
+      return findData;
+    }
+    throw new UnauthorizedException('Sorry, your password is invalid');
+  }
+}
